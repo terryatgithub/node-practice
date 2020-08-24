60909afa6121586bb215135d8841ecdf4d7ba9d1
yuanbo <yuanbo@coocaa.com>
2020-08-24 20:02:31 +0800

    node 3

---
+++ 20200820-node02/practice/src/index.js
@@ -0,0 +1,21 @@
+const Koa = require('koa')
+const app = new Koa()
+
+app.use(async (ctx, next) => {
+    const start = Date.now()
+    await next()
+    const end = Date.now()
+    console.log(`请求${ctx.url} 耗时${parseInt(end - start)}ms`);
+})
+
+app.use(async (ctx, next) => {
+    const expire = Date.now() + 102
+    while(Date.now() < expire) 
+    ctx.body = {
+        name: 'tom'
+    }
+})
+
+app.listen(3000, () => {
+    console.log('koa start on 3000...');
+})
