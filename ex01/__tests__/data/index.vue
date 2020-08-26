656c7641a7b7e4b3ea4b6174ccb74c89be84d78e
yuanbo <yuanbo@coocaa.com>
2020-08-26 10:36:48 +0800

    备份123

--- ex01/__tests__/data/index.vue
+++ ex01/__tests__/data/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="index">
+    <h1>This is an index page</h1>
   </div>
 </template>
--- ex03/index.js
+++ ex03/index.js
@@ -1,18 +1,24 @@
-const fs = require('fs')
+const fs = require('fs');
+const { rejects } = require('assert');
 module.exports.parser = path => {
     const readStream = fs.createReadStream(path)
     let reqData = [];
     let size = 0;
-    return new Promise(resolve => {
-         // ##BEGIN## 代码已加密
-JEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOOIOSOOJSOEAOEXOSSOOIOEOJHOOEEOESJPPJPEOSOOOIOEAOOIJPEJPAJEHOSOOOIOEAOOIJEHJXIJXAJEHOPX
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOEHJIHOOIOEAOOIJHOOEPOPJOEIOSHJPPOSOOOIOEAOOIJPHJXH
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEIOSXOPHOSSJEHJPIJXIJEHOSOOOIOEAOOIJHOOEJOSSOESOSPOEAOSHJXH
-JEHJEHJEHJEHJEHJEHJEHJEHOPAJPHJXH
-JEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOOIOSOOJSOEAOEXOSSOOIOEOJHOOEEOESJPPJPEOSSOESOSOJPEJPAJEHOSEOPJOESOSJOEAOSXOEEOESJEHJPPJPHJEHOPX
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOSJOEEOESOEIOEAJEHOSOOOIOEAOOIJEHJXIJEHJIEOPJOSEOSEOSSOEXJHOOSJOEEOESOSJOOIOEAJPPOEXOSSOEHJIHOOIOEAOOIJPAJEHOEIOSXOPHOSSJPHJXH
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOEIOEEOEJOPOOSSJPPJASOJSJAIJAXJHOOEPOOIOEXOEIOSSJPPOSOOOIOEAOOIJHOOEAOEEOJSOEAOEXOSXOESOSPJPPJPHJPHJPH
-JEHJEHJEHJEHJEHJEHJEHJEHOPAJPHJXH
-         // ##END##
+    return new Promise((resolve, reject) => {
+        //@暗号 二分查找
+        readStream.on('data', chunk => {
+            console.log('>>> data: ', chunk);
+            reqData.push(chunk)
+            size += chunk.length
+        })
+        readStream.on('end', () => {
+            const data = Buffer.concat(reqData, size)
+            console.log('end: ', size, ' ', data.toString());
+            resolve(JSON.parse(data))
+        })
+        readStream.on('error', err => {
+            console.log('err: ', JSON.stringify(err));
+            reject(err)
+        })
     })
 }
